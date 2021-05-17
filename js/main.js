fe851dddb60474067cc0cc3a3f0d404f02008cfa
journy002 <journy002@naver.com>
2021-05-17 22:13:07 +0900

    origin

---
+++ js/main.js
@@ -0,0 +1,66 @@
+// header 상단 input 기능 구현
+const searchEl = document.querySelector('.search');
+const searchForm = document.querySelector('.search-form');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function() {
+    searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function() {
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    searchInputEl.value = '';
+    console.log('submit');
+});
+
+// input에 focus가 해제되면 placeholder 제거를 위해 'blur'설정
+searchInputEl.addEventListener('blur', function() {
+    searchEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder', '');
+});
+
+// scroll시 배지 나타나고 사라지게 하기
+const badgeEl = document.querySelector('header .badges');
+// 스크롤시 함수 수십개가 실행되는데
+// lodash에서 제공하는 throttle() 기능을 이용하여 300(0.3초)부하를 시켜 부하를 막는다
+// throttle()은 scroll 사용시 매우 빈번하게 사용됨.
+// _.throttle(함수,시간)
+window.addEventListener('scroll', _.throttle(function(){
+    if(window.scrollY > 500) {
+        // 배지 숨기기
+        // gsap.to(요소, 지속시간, 옵션);
+        gsap.to(badgeEl, .6, {
+            display: 'none',
+            opacity: 0
+        });
+    } else {
+        // 배지 보이기
+        gsap.to(badgeEl, .6, {
+            display: 'block',
+            opacity: 1
+        })
+    }
+},300));
+
+
+// visual image,text 순차적으로 나타나게 만들기
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index) {
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) * .7, // 0.7 1.4 2.1 2.8
+        opacity: 1,
+    });
+});
+
+
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: false
+});
